44103f9cd422d930187fee2ef22ae330dc87442e
Kaolin <79109827+KaolinKiiro@users.noreply.github.com>
2022-10-11 10:59:03 -0400

    New changes

--- app/views/Main/orderPage.php
+++ app/views/Main/orderPage.php
@@ -5,8 +5,8 @@
     <link rel="stylesheet" href="/app/resources/styles/main.css">
     <link rel="stylesheet" href="/app/resources/styles/order.css">
 </head>
-<body class="homePage">
-    <img class="logo" src="/app/resources/images/logo.png" alt="">
+<body>
+    <!-- <img class="logo" src="/app/resources/images/logo.png" alt=""> -->
 
     <!-- <div class="appDivision clientNameDiv">
         <form action="" method="get">
@@ -14,10 +14,13 @@
             <span onclick="submitClientName()">Next</span>
         </form>
     </div> -->
-    <div class="appDivision laneNumberDiv">
-        <label>Lane Number: <input type="text" pattern="[0-9]*" inputmode="numeric" id="laneNumber"></label>
-    </div>
-    <div class="appDivision orderItems"></div>
+    <section class="appDivision laneNumberDiv" style="display: flex;">
+        <label>Lane Number: <input type="text" pattern="[0-9]*" inputmode="numeric" id="laneNumberInput"></label>
+        <label>Name: <input type="text" id="nameInput"></label>
+    </section>
+    <section class="appDivision orderItems">
+        <label>Items: <input type="text" pattern="[0-9]*" inputmode="numeric" id="laneNumber"></label>
+    </section>
 
     <script type="text/javascript" src="/app/resources/scripts/main.js"></script>
 </body>
